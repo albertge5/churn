3e23c13a8a4cf8b20605356574aceb8a33f31b86
albertge.lynbrook@gmail.com <age@caltech.edu>
2016-02-08 03:34:10 -0800

    done with first two map/reduce

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -1,10 +1,58 @@
 #!/usr/bin/env python
 
 import sys
+import numpy
 
 #
-# This program simply represents the identity function.
-#
 
+# parses the raw input
 for line in sys.stdin:
-    sys.stdout.write(line)
+    res = line.split('\t')
+    node = res[0]
+    lst = res[1].split(',')
+    curr_rank = lst[0]
+    prev_rank = lst[1]
+    adj_nodes = lst[2:]
+
+    for link in adj_nodes:
+        new_rank = float(curr_rank)/len(adj_nodes)
+        # emit the amount of rank a node contributes to a neighbor,
+        # and remember this node
+        result = str(link).rstrip() + "\t" + str(new_rank) + ","\
+            + str(node[7:]) + "\n"
+        sys.stdout.write(result)
+
+        # its to-be previous rank
+    result2 = str(node[7:]).rstrip() + "\t" + str(curr_rank) + "\n"
+    sys.stdout.write(result2)
+
+
+# dict = {}
+# for line in sys.stdin:
+#     res = line.split('\t')
+#     node = res[0][7:]
+#     adj = res[1].split(',')
+#     dict[str(node)] = adj
+#
+# # construct a matrix
+# num_nodes = len(dict)
+# mat = [[0.0] * num_nodes for _ in range(num_nodes)]
+# for key, value in dict.iteritems():
+#     num_adj = (len(value)) - 2
+#     print key
+#     for val in value[2:]:
+#         mat[int(key)][int(val)] = float(value[0])/num_adj
+#
+#
+# numpy.dot(
+
+
+
+
+
+
+
+
+
+
+
--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -6,6 +6,45 @@ import sys
 # This program simply represents the identity function.
 #
 
+alpha = 0.85
+# this reducer's particular key, since 
+# keys are grouped into contiguous blocks
+key = ""
+rank = 0.0
+old_rank = 0.0
+adj_list = []
+
 for line in sys.stdin:
-    sys.stdout.write(line)
+
+    res = line.split('\t')
+    node = res[0]
+    value = res[1].split(',')
+    if len(value) > 1:
+        contrib = float(value[0])
+        contrib_by = (value[1]).rstrip()
+
+        if node != key:
+            if key == "":
+                pass
+            else:
+                # moved onto a new block
+                # emit the rank of the previous block
+                # here we're using the matrix G = alpha*P + (1-alpha)/n * 1_(n*n)
+                # and this is where we finalize the computation pi = piG
+                # the second term contributes 1-alpha to every element in the vector
+                # of pi, and the first term just multiplies the sum
+                # of each node's contribution to an element by alpha
+                rank = alpha * (rank) + (1 - alpha)
+                sys.stdout.write("NodeId:" + key +\
+                        "\t" + str(rank) + "," + str(old_rank) + "," +\
+                        ",".join(adj_list) + "\n")
+                # reset rank, adj_list
+                rank = 0.0
+                adj_list = []
+            key = node
+        rank += float(contrib)
+        adj_list.append(contrib_by)
+    else:
+        old_rank = float(value[0])
+
 
